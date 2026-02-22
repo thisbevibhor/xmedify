4f4dc90edc4410380d69d7bca702a6aa1ea0251f
DeveloperInProgress <thisbevibhor@gmail.com>
2026-02-22 17:30:00 +0530

    Build home page with offers, specializations, specialists, blogs, and FAQs

---
+++ src/Home/Home.jsx
@@ -0,0 +1,63 @@
+import { Container, Box, Stack } from "@mui/material";
+import HeroSlider from "../components/HeroSlider/HeroSlider";
+import SearchHospital from "../components/SearchHospital/SearchHospital";
+import FAQs from "../components/Sections/FAQs/FAQs";
+import OurFamilies from "../components/Sections/OurFamilies/OurFamilies";
+import Blogs from "../components/Sections/Blogs/Blogs";
+import PatientCaring from "../components/Sections/PatientCaring/PatientCaring";
+import Specialists from "../components/Sections/Specialists/Specialists";
+import Specialization from "../components/Sections/Specialization/Specialization";
+import Offers from "../components/Sections/Offers/Offers";
+import NavBar from "../components/NavBar/NavBar";
+import HeroServices from "../components/IconLayout/HeroServices";
+
+// Home page - Landing page of the Medify application
+// Layout: NavBar + Hero section (slider, search, services) + Content sections
+export default function Home() {
+  return (
+    <Box>
+      {/* Hero area with gradient background */}
+      <Box
+        sx={{
+          background:
+            "linear-gradient(#E7F0FF , rgba(232, 241, 255, 0.47) 90%, #fff 10%)",
+        }}
+        mb={4}
+      >
+        <NavBar />
+        <Container maxWidth="xl">
+          <HeroSlider />
+          {/* Search and services card - positioned to overlap with hero using negative margin */}
+          <Stack
+            p={{ xs: 2.5, md: 8 }}
+            mt={{ xs: -2, md: 0, lg: -6, xl: -10 }}
+            position="relative"
+            zIndex={99}
+            bgcolor="#fff"
+            borderRadius="15px"
+            spacing={10}
+            boxShadow="0 0 12px rgba(0,0,0,0.1)"
+          >
+            <SearchHospital />
+            <HeroServices />
+          </Stack>
+        </Container>
+      </Box>
+
+      {/* Content sections rendered below the hero */}
+      <Offers />
+
+      <Specialization />
+
+      <Specialists />
+
+      <PatientCaring />
+
+      <Blogs />
+
+      <OurFamilies />
+
+      <FAQs />
+    </Box>
+  );
+}
